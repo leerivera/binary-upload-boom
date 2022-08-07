c34059ec56c15e9e0217ecd3d21397a2c6e628c8
leerivera <leerivera80@gmail.com>
2022-08-07 09:35:17 -0400

    yo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
-//////
+////// require("dotenv").config({ path: "./config/.env" });
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
 
